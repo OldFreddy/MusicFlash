a1dc7af60de9de2995a3b72f382224856f9987a6
OldFreddy <oldfredddy@gmail.com>
2019-07-02 14:23:02 +0300

    Добавьте файлы проекта.

---
+++ MusicFlash/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace MusicFlash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(13, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(168, 498);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(388, 245);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(95, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Откуда/Куда";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(388, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(388, 78);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(320, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(385, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Количество файлов";
+            this.label1.Visible = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(668, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Объем выбранных файлов";
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(385, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Свободное место";
+            this.label3.Visible = false;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(187, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(166, 498);
+            this.listBox2.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(388, 283);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 32);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "RANDOMIZER";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(385, 380);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Количество файлов в директории";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(388, 396);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            2048,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 11;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(388, 321);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 32);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Start";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(385, 442);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "label5";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(13, 566);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(754, 23);
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(645, 245);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "EXIT";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(732, 603);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "label6";
+            this.label6.Visible = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(13, 600);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "label7";
+            this.label7.Visible = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(385, 134);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(83, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Объем файлов";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(786, 625);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ MusicFlash/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO; //Подключаем пространство имен для работы с файлами
+using System.Collections; //подключаем коллекции для создания динамического массива
+
+
+namespace MusicFlash
+{
+
+    public partial class Form1 : Form
+    {
+        public string HomeFolder; //Переменная, содержащая в себе директорию с музыкой пользователя
+        public string FlashFolder; //Переменная, содержащая в себе директорию назначения
+        public string[] filenameswithdir; //массив имен файлов с директориями
+        public string[] filenames; //массив имен файлов без директорий
+        public double allsize; //переменная для рассчета объема музыки в дирректории
+        public double FreeSpace; //переменная для определения свободного места на съемном диске
+        public double FilesSize; //переменная содержащая объем, занимаемый всеми выбранными файлами
+        public string[] randfiles; //массив созданный случайно из массива имен файлов с путем
+        public int[] massrnd; //массив для хранения пройденных циклов рандома
+        public string[] filenamesSORT; //массив имен файлов без путей отсортированный
+        public double FolderCount;
+        public int filesInDir; //переменная количества файлов в директории
+        public int FolderName;
+        public string[] testmass;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear(); //очистка первого листбокса
+
+            /////Выбор директории с музыкой/////
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "Выберите папку с музыкой";
+            dialog.ShowNewFolderButton = false;
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                HomeFolder = dialog.SelectedPath;
+                textBox1.Text = HomeFolder;
+
+                /////Получение списка файлов в директории/////
+
+                filenameswithdir = Directory.GetFiles(HomeFolder, "*.mp3", SearchOption.AllDirectories); //получение списка файлов в указанной папке и подпапках
+                filenames = new string [filenameswithdir.Length]; //создание массива, в котором будут содержаться имена файлов и указание его длины
+                label1.Visible = true;
+                label1.Text = ((filenameswithdir.Length) + " Files"); //подсчет количества файлов в директории
+                allsize = 0;
+                for (int i = 0; i < filenameswithdir.Length; i++)
+                {
+                    filenames[i] = Path.GetFileName(filenameswithdir[i]); //получение имени файла без пути
+                    listBox1.Items.Add(filenames[i]); //запись имени файла в листбокс
+                    FileInfo size = new FileInfo(filenameswithdir[i]); //объявление обработки размера файла
+                    allsize = allsize + size.Length; //рассчет общего объема файлов в директории и поддиректориях
+
+
+
+                }
+                button2.Enabled = true;
+            }
+            /////////////////////////////////////
+
+           
+
+            double fullsize = allsize/1024/1024/1024; //перевод из байтов в гигабайты
+            label2.Visible = true;
+            label2.Text = fullsize.ToString("F") + " Gb"; //запись в лейбл в гигабайтах
+
+            //////////////////////////////////////////////
+            /////Вычисление свободного места на съемном диске и выбор директории/////
+
+
+            FolderBrowserDialog dialog1 = new FolderBrowserDialog();
+            dialog1.Description = "Выберите свою флешку";
+            dialog1.ShowNewFolderButton = false;
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                FlashFolder = dialog1.SelectedPath;
+                textBox2.Text = FlashFolder;
+
+                DriveInfo di = new DriveInfo(FlashFolder);
+                FreeSpace = di.AvailableFreeSpace;// 1024 / 1024 / 1024;
+                // MessageBox.Show(FreeSpace.ToString() + " Gb");
+                label3.Visible = true;
+                label3.Text = FreeSpace.ToString() + " Gb Free" ;
+                //label3.Text = Convert.ToString(FreeSpace);
+            }
+
+           
+
+            //////////////////////////////////////////////////////
+
+        }
+       
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+
+            //////////////////////////Генератор случайных уникальных чисел////////////////////
+            listBox2.Items.Clear(); //очистка листбокса
+            massrnd = new int [filenameswithdir.Length]; //присвоение массиву случайных чисел определенной длины
+            var rand = new Random(); //переменная, которая будет генерировать случайное число
+
+            bool alreadyThere; //переменная для проверки на существование похожего числа
+            for (int i = 0; i < filenameswithdir.Length; )
+            {
+                alreadyThere = false;
+                int newRandomValue = rand.Next(filenameswithdir.Length); // процедура получения случайного числа
+                for (int j = 0; j < i; j++)
+                {
+                    if (massrnd[j] == newRandomValue)
+                    {
+                        alreadyThere = true; //проверка на присутствие такого же числа
+                        break;
+
+                    }
+                }
+                if (alreadyThere != true)
+                {
+                    massrnd[i] = newRandomValue; //заполнение массива уникальным числом
+                    
+
+                    i++;
+                }
+
+                numericUpDown1.Enabled = true;
+            }
+            
+            /////Запись в листбокс отсортированных случайным образом имен файлов/////
+            filenamesSORT = new string[filenameswithdir.Length];
+            listBox2.Items.Clear();
+            FilesSize = 0;
+            
+
+            List<string> list = new List<string>(); //задаем динамический массив
+
+            for (int i = 0; i < filenameswithdir.Length; i++)
+            {
+                filenamesSORT[i] = Path.GetFileName(filenameswithdir[massrnd[i]]);
+                listBox2.Items.Add(filenamesSORT[i]);
+                FileInfo selectfile = new FileInfo(filenameswithdir[massrnd[i]]); // доступ к информации о выбранном в данный момент файле
+                FilesSize = FilesSize + selectfile.Length; //подсчет занимаемого файлами объема
+                
+
+                if (FilesSize < FreeSpace)
+                {
+            
+                    //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!Спросить у Жеки, почему тут появляется ошибка, если FileSize подсчет запихнуть сюда, а так же ,почему считает чуть больше, чем свободное место
+                    list.Add(filenameswithdir[massrnd[i]]); //записываем в динамический массив выбранный элемент
+                }
+                else
+                {
+                    label8.Text = Convert.ToString(FilesSize-selectfile.Length);
+                    break;
+                }
+                
+                
+            }
+            testmass = list.ToArray(); //конвертирование динамического массива в обычный
+            button3.Enabled = true;
+            progressBar1.Minimum = 0; //задаем минимум для прогрессбара
+            progressBar1.Maximum = testmass.Length; //задаем максимальное значение прогрессбара
+
+            //int g = filenamesSORT.Length;
+            //MessageBox.Show(filenamesSORT.Length.ToString());
+            ////////////////////////////////////////////////////////////////////////
+
+
+        }
+            ///////////////////Копирование отсортированных случайным образом файлов в директорию файлов в директорию/////////////////
+        private void button3_Click(object sender, EventArgs e)
+        {
+            filesInDir = (int)numericUpDown1.Value; //считывание значения количества файлов в директории из переключателя
+            label6.Visible = true;
+            label7.Visible = true;
+                                                  
+
+            for (int i = 0; i < testmass.Length; i++)
+            {
+
+                FolderName = 1 + i / filesInDir;
+                if (!Directory.Exists(FlashFolder + "/" + FolderName)) //проверка на существование директории
+                {
+                    Directory.CreateDirectory(FlashFolder + "/" + FolderName); //создание директории
+                }
+                if (!File.Exists(FlashFolder + "/" + FolderName + "/" + Path.GetFileName(testmass[i])))
+
+                {
+                    File.Copy(testmass[i], FlashFolder + "/" + FolderName + "/" + i + Path.GetFileName(testmass[i])); // копирование файлов
+                    progressBar1.Value = progressBar1.Value + 1; //увеличение прогресбара
+                    int x = progressBar1.Value * 100 / testmass.Length;
+                    label6.Text = x.ToString() + " %";  //проценты прогресса
+                    label7.Text = Path.GetFileName(testmass[i]); //отображение текущего копируемого файла
+                    Application.DoEvents(); //прерывание, чтобы не зависала оболочка
+                }
+ 
+                
+            }
+            MessageBox.Show("Копирование завершено");
+            ////////////////////////////////////////////////////////////////////////
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            string a = filenamesSORT.Length.ToString(); //общее число файлов
+            string b = numericUpDown1.Value.ToString(); //указанное пользователем число файлов в директории
+            FolderCount = Math.Ceiling(Convert.ToDouble(a) / Convert.ToDouble(b)); //рассчет количества директорий 
+            label5.Text = (Convert.ToString(FolderCount) + "директорий");
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill(); //принудительная остановка процесса
+        }
+    }
+}
